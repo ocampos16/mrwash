29099a8e0a453f697c30e780afdac2e41fbfa41d
ocampos16 <oc_campos16@hotmail.com>
2019-12-13 14:50:46 +0100

    First commit on master

---
+++ js/book_success.js
@@ -0,0 +1,10 @@
+$(function() {
+
+	 document.getElementById("submitButton").onclick = function () {
+       window.location.replace("booking-complete.html");
+       return false;
+
+    };
+
+  
+})	;
